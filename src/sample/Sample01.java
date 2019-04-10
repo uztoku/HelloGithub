138270a3e98aceda66b6f2433a5cef9ed6e6f5ad
uztoku <yuuji.tokutake@gmail.com>
2019-04-10 23:48:36 +0900

    Add Math.random()

--- src/sample/Sample01.java
+++ src/sample/Sample01.java
@@ -6,6 +6,8 @@ public class Sample01 {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello World!!");
 		System.out.println("Hello Github!!");
+		double number = Math.random();
+		System.out.println(number);
 	}
 
 }
